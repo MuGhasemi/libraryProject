ba611bcdbf8b6b87ff41c97f789a48a283d114b5
MuGhasemi <mughasemi2008@gmail.com>
2023-07-20 21:20:42 +0330

    fix detail page

---
+++ templates/static/asets/JS/app.js
@@ -0,0 +1,13 @@
+document
+    .querySelector("#sin-btn-form")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+    });
+
+//   login-btn-form
+
+document
+    .querySelector("#login-btn-form")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+    });
