88c5d08c28dbacb03908c4bc39f890505dbe3c21
Muhammad <104085463+MuGhasemi@users.noreply.github.com>
2023-08-03 17:56:10 +0330

    Installed and configured jalali_date package

--- library/settings.py
+++ library/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'book.apps.BookConfig',
     'account.apps.AccountConfig',
     'sweetify',
+    'jalali_date',
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -143,3 +145,28 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRON_CLASSES = [
     'path.to.DeleteExpiredInstances',
 ]
+
+# default settings (optional)
+JALALI_DATE_DEFAULTS = {
+   'Strftime': {
+        'date': '%y/%m/%d',
+        'datetime': '%H:%M:%S _ %y/%m/%d',
+    },
+    'Static': {
+        'js': [
+            # loading datepicker
+            'admin/js/django_jalali.min.js',
+            # OR
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/calendar.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js',
+            # 'admin/js/main.js',
+        ],
+        'css': {
+            'all': [
+                'admin/jquery.ui.datepicker.jalali/themes/base/jquery-ui.min.css',
+            ]
+        }
+    },
+}
